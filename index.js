d0a4a882d73626aa7c2a05a9b6ec505b7eec81cd
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-01-25 00:07:35 -0300

    Add - modo dark

---
+++ index.js
@@ -0,0 +1,50 @@
+const Modal = {
+  open(){
+    document
+      .querySelector('.modal-overlay')
+      .classList
+      .add('active')
+  },
+  close(){
+    document
+      .querySelector('.modal-overlay')
+      .classList
+      .remove('active')
+  }
+}
+
+let COLOR_THEME = window
+  .matchMedia("(prefers-color-scheme: light)")
+  .matches
+  ? 'light'
+  : 'dark'
+
+function switchTheme() {
+  const currentTheme = COLOR_THEME
+
+  COLOR_THEME = COLOR_THEME === 'light'
+    ? 'dark'
+    : 'light'
+
+  const rules = window.document.styleSheets[0].cssRules
+
+  document.querySelector('#img-total').src = `./assets/total-${COLOR_THEME}.svg`
+ 
+  for (i = 0; i < rules.length; i++) {
+    let item
+    media = rules[i].media
+
+    if (media == undefined) {
+      continue
+    }
+
+    item = media
+      .mediaText
+      .replace(
+        "(prefers-color-scheme: " + COLOR_THEME + ")", 
+        "(prefers-color-scheme: " + currentTheme + ")"
+      )
+
+    media.mediaText = item
+  }
+}
