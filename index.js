ddc76e82644a9d274c4b2608c9c6684f920afc03
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-02-03 19:12:54 -0300

    Add - preettier comfig e refatorando forma de pegar data

--- index.js
+++ index.js
@@ -1,151 +1,152 @@
-const wallets = ["Pessoal", "Empresa"];
+const wallets = ['Pessoal', 'Empresa']
 
 const Modal = {
   toggle(value) {
     document
       .querySelector(`.modal-overlay.${value}`)
-      .classList.toggle("active");
+      .classList.toggle('active')
   },
-};
+}
 
 const Storage = {
   get() {
-    return JSON.parse(localStorage.getItem(`dev.finances:wallets`)) || [];
+    return (
+      JSON.parse(localStorage.getItem(`dev.finances:wallets`)) || []
+    )
   },
 
   set(wallets) {
-    localStorage.setItem(`dev.finances:wallets`, JSON.stringify(wallets));
+    localStorage.setItem(
+      `dev.finances:wallets`,
+      JSON.stringify(wallets)
+    )
   },
-};
+}
 
 const Wallet = {
   all: Storage.get(),
-  selected: Storage.get()[0] || Modal.toggle("modal-wallets"),
+  selected: Storage.get()[0] || Modal.toggle('modal-wallets'),
   index: 0,
 
   update() {
-    Wallet.all[Wallet.index] = Wallet.selected;
+    Wallet.all[Wallet.index] = Wallet.selected
   },
 
   add(wallet) {
-    if (!wallet.transactions) wallet.transactions = [];
-    Wallet.all.push(wallet);
+    if (!wallet.transactions) wallet.transactions = []
+    Wallet.all.push(wallet)
   },
 
   remove(index) {
-    Wallet.all.splice(index, 1);
-    App.reload();
+    Wallet.all.splice(index, 1)
+    App.reload()
   },
 
   select(index) {
-    Modal.toggle("modal-wallets");
+    Modal.toggle('modal-wallets')
 
-    Wallet.selected = Wallet.all[index];
-    Transaction.all = Wallet.selected;
-    Wallet.index = index;
+    Wallet.selected = Wallet.all[index]
+    Transaction.all = Wallet.selected
+    Wallet.index = index
 
-    App.reload();
+    App.reload()
   },
 
   export() {
-    const wallet = JSON.stringify(Wallet.selected);
+    const wallet = JSON.stringify(Wallet.selected)
     Utils.downloadFile(
       wallet,
       `wallet-${Wallet.selected?.name}.json`,
-      "application/json"
-    );
-    return;
+      'application/json'
+    )
+    return
   },
 
   extract() {
-    const transactions = Transaction.all.transactions;
-    const incomes = Transaction.incomes();
-    const expenses = Transaction.expenses();
-    const total = Transaction.total();
+    const transactions = Transaction.all.transactions
+    const incomes = Transaction.incomes()
+    const expenses = Transaction.expenses()
+    const total = Transaction.total()
 
-    const currentDate = new Date();
+    const currentDate = new Date()
+    const date = currentDate.toLocaleDateString('pt-br')
+    const time = currentDate.toLocaleTimeString('pt-br')
 
-    const date = {
-      day: currentDate.getDay(),
-      month: currentDate.getMonth() + 1,
-      year: currentDate.getFullYear(),
-      hours: currentDate.getHours(),
-      minutes: currentDate.getMinutes(),
-      seconds: currentDate.getSeconds(),
-    };
-
-    let text = `Extrato - Data: ${`${date.day}/${date.month}/${date.year} - ${date.hours}:${date.minutes}:${date.seconds}\n`}`;
+    let text = `Extrato - Data: ${`${date} - ${time}\n`}`
     text += transactions.reduce(
       (txt, transaction) =>
         (txt += `\n${transaction.date} - ${
           transaction.description
         }       ${Utils.formatCurrency(transaction.amount)}`),
-      ""
-    );
-    text += `\n\nEntradas:        ${Utils.formatCurrency(incomes)}`;
-    text += `\nSaídas:          ${Utils.formatCurrency(expenses)}`;
-    text += `\nTotal:           ${Utils.formatCurrency(total)}`;
+      ''
+    )
+    text += `\n\nEntradas:        ${Utils.formatCurrency(incomes)}`
+    text += `\nSaídas:          ${Utils.formatCurrency(expenses)}`
+    text += `\nTotal:           ${Utils.formatCurrency(total)}`
 
-    Utils.downloadFile(text, "extrato.txt", "application/text");
+    Utils.downloadFile(text, 'extrato.txt', 'application/text')
   },
-};
+}
 
 const Transaction = {
   all: Wallet.selected || [],
 
   add(transaction) {
-    Transaction.all.transactions.push(transaction);
-    App.reload();
+    Transaction.all.transactions.push(transaction)
+    App.reload()
   },
 
   remove(index) {
-    Transaction.all.transactions.splice(index, 1);
-    App.reload();
+    Transaction.all.transactions.splice(index, 1)
+    App.reload()
   },
 
   incomes(transactions = Transaction.all.transactions) {
     return transactions?.reduce(
       (total, { amount }) => (amount > 0 ? amount + total : total),
       0
-    );
+    )
   },
 
   expenses(transactions = Transaction.all.transactions) {
     return transactions?.reduce(
       (total, { amount }) => (amount < 0 ? amount + total : total),
       0
-    );
+    )
   },
 
   total(transactions = Transaction.all.transactions) {
-    return transactions?.reduce((total, { amount }) => amount + total, 0);
+    return transactions?.reduce(
+      (total, { amount }) => amount + total,
+      0
+    )
   },
-};
+}
 
 const DOM = {
-  transactionsContainer: document.querySelector("#data-table tbody"),
-  walletsContainer: document.querySelector("#wallets-table tbody"),
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  walletsContainer: document.querySelector('#wallets-table tbody'),
 
   addWallet(wallet, index) {
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr')
 
-    tr.innerHTML = DOM.innerHTMLWallet(wallet, index);
-    tr.dataset.index = index;
+    tr.innerHTML = DOM.innerHTMLWallet(wallet, index)
+    tr.dataset.index = index
 
-    DOM.walletsContainer.appendChild(tr);
+    DOM.walletsContainer.appendChild(tr)
   },
 
   innerHTMLWallet(wallet, index) {
-    const { name, transactions } = wallet;
+    const { name, transactions } = wallet
 
     const amount = transactions?.reduce(
       (current, next) => current + next.amount,
       0
-    );
+    )
 
-    const CSSClass = amount > 0 ? "income" : "expense";
+    const CSSClass = amount > 0 ? 'income' : 'expense'
 
-    const newAmount = Utils.formatCurrency(amount);
+    const newAmount = Utils.formatCurrency(amount)
 
     const html = `
     <td onclick="Wallet.select(${index})" class="name button">${name}</td>
@@ -153,24 +154,24 @@ const DOM = {
     <td>
       <img class="button" onclick="Wallet.remove(${index})" src="./assets/minus.svg" alt="Remover carteira">
     </td>
-    `;
-    return html;
+    `
+    return html
   },
 
   addTransaction(transaction, index) {
-    const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
-    tr.dataset.index = index;
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    tr.dataset.index = index
 
-    DOM.transactionsContainer.appendChild(tr);
+    DOM.transactionsContainer.appendChild(tr)
   },
 
   innerHTMLTransaction(transaction, index) {
-    const { description, amount, date } = transaction;
+    const { description, amount, date } = transaction
 
-    const CSSClass = amount > 0 ? "income" : "expense";
+    const CSSClass = amount > 0 ? 'income' : 'expense'
 
-    const newAmount = Utils.formatCurrency(amount);
+    const newAmount = Utils.formatCurrency(amount)
 
     const html = `
     <td class="description">${description}</td>
@@ -179,220 +180,224 @@ const DOM = {
     <td>
       <img class="" onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
     </td>
-    `;
-    return html;
+    `
+    return html
   },
 
   updateBalance() {
-    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
-      Transaction.incomes()
-    );
+    document.getElementById(
+      'incomeDisplay'
+    ).innerHTML = Utils.formatCurrency(Transaction.incomes())
 
-    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
-      Transaction.expenses()
-    );
+    document.getElementById(
+      'expenseDisplay'
+    ).innerHTML = Utils.formatCurrency(Transaction.expenses())
 
-    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
-      Transaction.total()
-    );
+    document.getElementById(
+      'totalDisplay'
+    ).innerHTML = Utils.formatCurrency(Transaction.total())
   },
 
   clearTransactions() {
-    DOM.transactionsContainer.innerHTML = "";
+    DOM.transactionsContainer.innerHTML = ''
   },
 
   clearWallets() {
-    DOM.walletsContainer.innerHTML = "";
+    DOM.walletsContainer.innerHTML = ''
   },
-};
+}
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value) * 100;
-    return Math.round(value);
+    value = Number(value) * 100
+    return Math.round(value)
   },
 
   formatDate(date) {
-    const [year, month, day] = date.split("-");
-    return `${day}/${month}/${year}`;
+    const [year, month, day] = date.split('-')
+    return `${day}/${month}/${year}`
   },
 
   formatCurrency(value) {
-    const signal = Number(value) < 0 ? "-" : "";
+    const signal = Number(value) < 0 ? '-' : ''
 
-    value = String(value).replace(/\D/g, "");
+    value = String(value).replace(/\D/g, '')
 
-    value = Number(value) / 100;
+    value = Number(value) / 100
 
-    value = value.toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    });
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    })
 
-    return signal + value;
+    return signal + value
   },
 
   downloadFile(data, name, type) {
     const blob = new Blob([data], {
       type: type,
-    });
-    const link = window.document.createElement("a");
-    link.href = window.URL.createObjectURL(blob);
-    link.download = `${name.trim().replace(/ +/g, "-")}`;
-    link.click();
-    window.URL.revokeObjectURL(link.href);
-    return;
+    })
+    const link = window.document.createElement('a')
+    link.href = window.URL.createObjectURL(blob)
+    link.download = `${name.trim().replace(/ +/g, '-')}`
+    link.click()
+    window.URL.revokeObjectURL(link.href)
+    return
   },
-};
+}
 
 const WalletForm = {
-  name: document.querySelector("input#wallet-name"),
+  name: document.querySelector('input#wallet-name'),
 
   getValues() {
-    return { name: WalletForm.name.value };
+    return { name: WalletForm.name.value }
   },
 
   validadeFields() {
-    const { name } = WalletForm.getValues();
-    if (name === "") {
-      throw new Error("Por favor, preencha todos os campos.");
+    const { name } = WalletForm.getValues()
+    if (name === '') {
+      throw new Error('Por favor, preencha todos os campos.')
     }
   },
 
   formatValues() {
-    let { name } = WalletForm.getValues();
-    return { name: name.replace(/ +/g, " ").trim() };
+    let { name } = WalletForm.getValues()
+    return {
+      name: name.replace(/ +/g, ' ').trim(),
+    }
   },
 
   saveWallet(wallet) {
-    Wallet.add(wallet);
+    Wallet.add(wallet)
   },
 
   clearFields() {
-    WalletForm.name.value = "";
+    WalletForm.name.value = ''
   },
 
   submit(event) {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      WalletForm.validadeFields();
-      const wallet = WalletForm.formatValues();
+      WalletForm.validadeFields()
+      const wallet = WalletForm.formatValues()
 
-      WalletForm.saveWallet(wallet);
+      WalletForm.saveWallet(wallet)
 
-      WalletForm.clearFields();
+      WalletForm.clearFields()
 
-      DOM.clearWallets();
-      Wallet.all.forEach(DOM.addWallet);
+      DOM.clearWallets()
+      Wallet.all.forEach(DOM.addWallet)
     } catch (error) {
-      alert(error.message);
+      alert(error.message)
     }
   },
-};
+}
 
 const Form = {
-  description: document.querySelector("input#description"),
-  amount: document.querySelector("input#amount"),
-  date: document.querySelector("input#date"),
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
 
   getValues() {
     return {
-      description: Form.description.value.replace(/ +/g, " ").trim(),
+      description: Form.description.value.replace(/ +/g, ' ').trim(),
       amount: Form.amount.value,
       date: Form.date.value,
-    };
+    }
   },
 
   validadeFields() {
-    const { description, amount, date } = Form.getValues();
+    const { description, amount, date } = Form.getValues()
     if (
-      description.trim() === "" ||
-      amount.trim() === "" ||
-      date.trim() === ""
+      description.trim() === '' ||
+      amount.trim() === '' ||
+      date.trim() === ''
     ) {
-      throw new Error("Por favor, preencha todos os campos.");
+      throw new Error('Por favor, preencha todos os campos.')
     }
   },
 
   formatValues() {
-    let { description, amount, date } = Form.getValues();
-    amount = Utils.formatAmount(amount);
-    date = Utils.formatDate(date);
+    let { description, amount, date } = Form.getValues()
+    amount = Utils.formatAmount(amount)
+    date = Utils.formatDate(date)
 
     return {
       description,
       amount,
       date,
-    };
+    }
   },
 
   saveTransaction(transaction) {
-    Transaction.add(transaction);
+    Transaction.add(transaction)
   },
 
   clearFields() {
-    (Form.description.value = ""),
-      (Form.amount.value = ""),
-      (Form.date.value = "");
+    ;(Form.description.value = ''),
+      (Form.amount.value = ''),
+      (Form.date.value = '')
   },
 
   submit(event) {
-    event.preventDefault();
+    event.preventDefault()
 
     try {
-      Form.validadeFields();
-      const transaction = Form.formatValues();
+      Form.validadeFields()
+      const transaction = Form.formatValues()
 
-      Form.saveTransaction(transaction);
+      Form.saveTransaction(transaction)
 
-      Form.clearFields();
+      Form.clearFields()
 
-      Modal.toggle("transaction");
+      Modal.toggle('transaction')
     } catch (error) {
-      alert(error.message);
+      alert(error.message)
     }
   },
-};
+}
 
 const App = {
   init() {
-    Transaction.all.transactions?.forEach(DOM.addTransaction);
-    Wallet.all.forEach(DOM.addWallet);
+    Transaction.all.transactions?.forEach(DOM.addTransaction)
+    Wallet.all.forEach(DOM.addWallet)
 
-    DOM.updateBalance();
-    Wallet.update();
+    DOM.updateBalance()
+    Wallet.update()
 
-    Storage.set(Wallet.all);
+    Storage.set(Wallet.all)
 
-    document.querySelector("#wallet-selected-name").innerHTML =
-      Transaction.all?.name || "Crie um Carteira";
+    document.querySelector('#wallet-selected-name').innerHTML =
+      Transaction.all?.name || 'Crie um Carteira'
   },
 
   reload() {
-    DOM.clearTransactions();
-    DOM.clearWallets();
-    this.init();
+    DOM.clearTransactions()
+    DOM.clearWallets()
+    this.init()
   },
-};
+}
 
-App.init();
+App.init()
 
 const invertTheme = (mediaText) =>
-  mediaText.indexOf("dark") > -1 ? ["dark", "light"] : ["light", "dark"];
+  mediaText.indexOf('dark') > -1
+    ? ['dark', 'light']
+    : ['light', 'dark']
 
 function switchTheme() {
-  const cssRules = window.document.styleSheets[0].cssRules;
+  const cssRules = window.document.styleSheets[0].cssRules
 
   for (const rule of cssRules) {
-    let media = rule.media;
+    let media = rule.media
 
     if (media) {
-      const [currentTheme, nextTheme] = invertTheme(media.mediaText);
+      const [currentTheme, nextTheme] = invertTheme(media.mediaText)
 
       media.mediaText = media.mediaText.replace(
-        "(prefers-color-scheme: " + currentTheme + ")",
-        "(prefers-color-scheme: " + nextTheme + ")"
-      );
+        '(prefers-color-scheme: ' + currentTheme + ')',
+        '(prefers-color-scheme: ' + nextTheme + ')'
+      )
     }
   }
 }
